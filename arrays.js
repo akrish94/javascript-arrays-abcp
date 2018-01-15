e1d04db30b988d5ff94227c2a9f4a98f8f009a04
Adithi Krishnamoorthy <adithikrishnamoorthy@gmail.com>
2018-01-15 19:58:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,7 +27,14 @@ function accessElementInArray(array,index) {
   return array[index]
 }
 
+
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
   return array
+}
+
+
+function removeElementFromBeginningOfArray(array){
+  var newArray = array.slice(1)
+  return newArray
 }
