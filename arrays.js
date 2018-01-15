9b68a372e34c3353f512983903dec7e9c304d3d6
Adithi Krishnamoorthy <adithikrishnamoorthy@gmail.com>
2018-01-15 19:56:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,3 +26,8 @@ function destructivelyAddElementToEndOfArray(array,element) {
 function accessElementInArray(array,index) {
   return array[index]
 }
+
+function destructivelyRemoveELementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
