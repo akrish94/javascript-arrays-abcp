43f085063242a8d9fbca805f7ea3f615957a07e6
Adithi Krishnamoorthy <adithikrishnamoorthy@gmail.com>
2018-01-15 20:00:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -38,3 +38,9 @@ function removeElementFromBeginningOfArray(array){
   var newArray = array.slice(1)
   return newArray
 }
+
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
